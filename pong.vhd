f422473f76d0c7800bf06088270e2f4aafe28732
Aaron Lee <45613337+Fungeey@users.noreply.github.com>
2024-11-07 10:33:36 -0500

    static image generation - border, players, background and basic movement

--- pong.vhd
+++ pong.vhd
@@ -26,13 +26,13 @@ architecture Behavioral of Pong is
    constant HFP : integer := 16;    -- horizontal front porch
    constant HSP : integer := 96;    -- horizontal sync pulse
    constant HBP : integer := 48;    -- horizontal back porch
-   constant SCREEN_W : integer := HD + HFP + HSP + HBP;
+   constant SCREEN_W : integer := HD + HFP + HSP + HBP; -- 800
 
    constant VD : integer := 480;    -- screen height
    constant VFP : integer := 10;    -- vertical front porc      
    constant VSP : integer := 2;     -- vertical sync pulse
    constant VBP : integer := 33;    -- vertical back porch
-   constant SCREEN_H : integer := VD + VFP + VSP + VBP;
+   constant SCREEN_H : integer := VD + VFP + VSP + VBP; -- 525
    
    -- clock signals
    signal clk_counter : integer := 0;
@@ -49,13 +49,18 @@ architecture Behavioral of Pong is
    signal G : std_logic_vector(7 downto 0);
    signal B : std_logic_vector(7 downto 0);
 
+   -- borders
+   constant BORDER : integer := 10;
+   constant MARGIN : integer := 25;
+
    -- player signals
    constant PADDLE_SPD : integer := 1;
-   constant PADDLE_HEIGHT : integer := 15;
-   constant P1_X : integer := 5;   -- initialize these
-   constant P2_X : integer := 0;   -- initialize these
+   constant PADDLE_WIDTH : integer := 10;
+   constant PADDLE_HEIGHT : integer := 100;
+   constant P1_X : integer := 50;   -- initialize these
+   constant P2_X : integer := 590;   -- initialize these
 
-   signal p1_y : integer := 240; --VD/2
+   signal p1_y : integer := VD/2; --VD/2
    signal p2_y : integer := VD/2;
 
    constant BALL_SPD : integer := 4;
@@ -64,6 +69,9 @@ architecture Behavioral of Pong is
    signal ball_dir_y : integer := 1;   -- -1 or 1
    signal ball_x : integer := HD/2;
    signal ball_y : integer := VD/2;
+
+   signal movementTick : std_logic;
+   signal movCounter : integer := 0;
 
 begin
    
@@ -77,7 +85,21 @@ begin
          else
             pxl_clk <= '0';
          end if;
+
       end if;
+   end process;
+
+   moveDelay: process(pxl_clk)
+   begin
+      movCounter <= movCounter + 1;
+
+      if(movCounter >= 90000) then
+         movementTick <= '1';
+         movCounter <= 0;
+      else
+         movementTick <= '0';
+      end if;
+
    end process;
 
    hvpos: process(pxl_clk)
@@ -97,7 +119,7 @@ begin
       end if;
    end process;
 
-   HVSync: process(pxl_clk, hpos, vpos)
+   HVSync: process(pxl_clk)
    begin
       if(pxl_clk'event and pxl_clk = '1') then
          if(hpos <= HD + HFP or hpos >= HD + HFP + HSP) then
@@ -114,7 +136,7 @@ begin
       end if;
    end process;
 
-   video : process(pxl_clk, hpos, vpos)
+   video : process(pxl_clk)
    begin
       if(pxl_clk'event and pxl_clk = '1') then
          if(hpos <= HD and vpos <= VD) then
@@ -125,24 +147,27 @@ begin
       end if;
    end process;
 
-   game: process(pxl_clk, SW0, SW1, SW2, SW3)
+   game: process(pxl_clk)
    begin
       if(pxl_clk'event and pxl_clk = '1') then
-         -- sw0: pause switch
--- and SW0 = '1'
+         -- sw2: pause switch
+         -- and SW2 = '1'
 
-         -- sw1: player 1
-         if(SW1 = '1' and p1_y >= 43) then
-            p1_y <= p1_y - PADDLE_SPD;
-         elsif(SW1 = '0' and p1_y + PADDLE_HEIGHT <= 440) then
-            p1_y <= p1_y + PADDLE_SPD;
-         end if;
+         if(movementTick = '1') then
 
-         -- sw2: player 2
-         if(SW2 = '1' and p2_y >= 43) then
-            p2_y <= p2_y - PADDLE_SPD;
-         elsif(SW2 = '0' and p2_y + PADDLE_HEIGHT <= 440) then
-            p2_y <= p2_y + PADDLE_SPD;
+            -- sw1: player 1
+            if(SW3 = '1' and p1_y >= MARGIN+BORDER + PADDLE_HEIGHT/2) then
+               p1_y <= p1_y - PADDLE_SPD;
+            elsif(SW3 = '0' and p1_y <= VD-MARGIN-BORDER- PADDLE_HEIGHT/2) then
+               p1_y <= p1_y + PADDLE_SPD;
+            end if;
+
+            -- sw2: player 2
+            if(SW0 = '1' and p2_y >= 43) then
+               p2_y <= p2_y - PADDLE_SPD;
+            elsif(SW0 = '0' and p2_y + PADDLE_HEIGHT <= 440) then
+               p2_y <= p2_y + PADDLE_SPD;
+            end if;
          end if;
 
          -- ball movement
@@ -154,44 +179,64 @@ begin
 
 
 
-         --sw3: reset
-         if(SW3'event and SW3 = '1') then
+
+         --sw2: reset
+         if(SW2'event and SW2 = '1') then
             -- reset variables
+            p1_y <= VD/2;
+            p2_y <= VD/2;
+            ball_x <= HD/2;
+            ball_y <= VD/2;
          end if;
 
       end if;
    end process;
 
-   draw: process(pxl_clk, video_on, hpos, vpos, p1_y)
+   draw: process(pxl_clk)
    begin
       if(pxl_clk'event and pxl_clk = '1' and video_on = '1') then
-         -- if outside screen, must output 1111111
-         -- else, display colors
-
-
-
-         -- edges
-         --if(hpos >= 25 and hpos <= 615 and ((vpos >= 34 and vpos <= 45) or (vpos >= 435 and vpos <= 446))) then
          if(hpos > HD or vpos > VD) then
-         -- all 1s = black in the porch
+            -- all 1s = white in the porch
+            -- if outside screen, must output 1111111
+            -- else, display colors
+            R <= "00000000";
+            G <= "00000000";
+            B <= "00000000";
+         elsif(hpos >= MARGIN and hpos <= HD-MARGIN and
+         ((vpos >= MARGIN and vpos <= MARGIN + BORDER) or
+         (vpos >= VD-MARGIN-BORDER and vpos <= VD-MARGIN))) then
+            -- horizontal borders
             R <= "11111111";
             G <= "11111111";
             B <= "11111111";
-         --and (vpos < p1_y + 5 or vpos > p1_y - 5)
-         elsif(hpos < 100) then
+         elsif(((hpos >= MARGIN and hpos <= MARGIN + BORDER) or (hpos >= 604 and hpos <= 615)) and
+         ((vpos >= 34 and vpos <= 112) or (vpos >= 370 and vpos <= 446))) then
+            -- vertical borders
             R <= "11111111";
             G <= "11111111";
             B <= "11111111";
-         elsif(hpos > 100) then
-            R <= "11110000";
-            G <= "11001111";
-            B <= "00001111";
+         elsif(hpos >= HD/2-2 and hpos <= HD/2+2 and (vpos mod 64 >= 32) and vpos >= 45 and vpos <= 435) then
+         -- black middle line
+            R <= "00000000";
+            G <= "00000000";
+            B <= "00000000";
+         elsif(hpos >= P1_X-PADDLE_WIDTH/2 and hpos <= P1_X+PADDLE_WIDTH/2 and vpos >= p1_y - PADDLE_HEIGHT/2 and vpos <= p1_y + PADDLE_HEIGHT/2) then
+         -- player 1
+            R <= "00000000";
+            G <= "00000000";
+            B <= "11111111";
+         elsif(hpos >= P2_X-PADDLE_WIDTH/2 and hpos <= P2_X+PADDLE_WIDTH/2 and vpos >= p2_y - PADDLE_HEIGHT/2 and vpos <= p2_y + PADDLE_HEIGHT/2) then
+            -- player 2
+            R <= "11111111";
+            G <= "00000000";
+            B <= "11111111";
+         else
+            R <= "00000000";
+            G <= "11111111";
+            B <= "00000000";
          end if;
 
-
-         
-
--- board
+         -- board
 
          -- paddle 1
 
